4050e8d58b65d4b7cd0835c7462eb6dd67559a7c
Aaron <Aaron@Mav3r1ck.io>
2016-05-07 02:33:08 -0400

    BoutTime Project 3

---
+++ BoutTime/HistoricalEvent.swift
@@ -0,0 +1,82 @@
+//
+//  HistoricalEvent.swift
+//  BoutTime
+//
+//  Created by Aaron on 5/3/16.
+//  Copyright © 2016 Aaron A. All rights reserved.
+//
+
+import Foundation
+import GameplayKit
+
+class HistoricalEvent: Equatable, CustomStringConvertible {
+    let event: String
+    let date: Int
+    
+    var description: String {
+        return "\(event)"
+    }
+    
+    init(event: String, date: Int) {
+        self.event = event
+        self.date = date
+    }
+}
+
+
+func == (lhs: HistoricalEvent, rhs: HistoricalEvent) -> Bool {
+    return lhs.date == rhs.date && lhs.event == rhs.event
+}
+
+// Data Set
+let georgeWashington = HistoricalEvent(event:"George Washington", date: 1789)
+let johnAdams = HistoricalEvent(event:"John Adams", date: 1797)
+let thomasJefferson = HistoricalEvent(event:"Thomas Jefferson", date: 1801)
+let jamesMadison = HistoricalEvent(event:"James Madison", date: 1809)
+let jamesMonroe = HistoricalEvent(event:"James Monroe", date: 1817)
+let johnQuincyAdams = HistoricalEvent(event:"John Quincy Adams", date: 1825)
+let andrewJackson = HistoricalEvent(event:"Andrew Jackson", date: 1829)
+let martinVanBuren = HistoricalEvent(event:"Martin Van Buren", date: 1837)
+let williamHenryHarrison = HistoricalEvent(event:"William Henry Harrison", date: 1841)
+let johnTyler = HistoricalEvent(event:"John Tyler", date: 1842)
+let jamesKPolk = HistoricalEvent(event:"James K. Polk", date: 1845)
+let zacharyTaylor = HistoricalEvent(event:"Zachary Taylor", date: 1849)
+let millardFillmore = HistoricalEvent(event:"Millard Fillmore", date: 1850)
+let franklinPierce = HistoricalEvent(event:"Franklin Pierce", date: 1853)
+let jamesBuchanan = HistoricalEvent(event:"James Buchanan", date: 1857)
+let abrahamLincoln = HistoricalEvent(event:"Abraham Lincoln", date: 1861)
+let andrewJohnson = HistoricalEvent(event:"Andrew Johnson", date: 1865)
+let ulyssesSGrant = HistoricalEvent(event:"Ulysses S. Grant", date: 1869)
+let rutherfordBHayes = HistoricalEvent(event:"Rutherford B. Hayes", date: 1877)
+let jamesAGarfield = HistoricalEvent(event:"James A. Garfield", date: 1881)
+let chesterAArthur = HistoricalEvent(event:"Chester A Arthur", date: 1882)
+let groverCleveland1 = HistoricalEvent(event:"Grover Cleveland 1st term", date: 1885)
+let benjaminHarrison = HistoricalEvent(event:"Benjamin Harrison", date: 1889)
+let groverCleveland2 = HistoricalEvent(event:"Grover Cleveland 2nd term", date: 1893)
+let williamMckinley = HistoricalEvent(event:"William McKinley", date: 1897)
+let theodoreRoosevelt = HistoricalEvent(event:"Theodore Roosevelt", date: 1901)
+let williamHowardTaft = HistoricalEvent(event:"William Howard Taft", date: 1909)
+let woodrowWilson = HistoricalEvent(event:"Woodrow Wilson", date: 1913)
+let warrenGHarding = HistoricalEvent(event:"Warren G Harding", date: 1921)
+let calvinCoolidge = HistoricalEvent(event:"Calvin Coolidge", date: 1923)
+let herbertHoover = HistoricalEvent(event:"Herbert Hoover", date: 1929)
+let franklinDRoosevelt = HistoricalEvent(event:"Franklin D Roosevelt", date: 1933)
+let harrySTruman = HistoricalEvent(event:"Harry S Truman", date: 1945)
+let dwightDEisenhower = HistoricalEvent(event:"Dwight D Eisenhower", date: 1953)
+let johnFKennedy = HistoricalEvent(event:"John F Kennedy", date: 1961)
+let lyndonBJohnson = HistoricalEvent(event:"Lyndon B. Johnson", date: 1963)
+let richardNixon = HistoricalEvent(event:"Richard Nixon", date: 1969)
+let geraldFord = HistoricalEvent(event:"Gerald Ford", date: 1974)
+let jimmyCarter = HistoricalEvent(event:"Jimmy Carter", date: 1977)
+let ronaldReagan = HistoricalEvent(event:"Ronald Reagan", date: 1981)
+let georgeHWBush = HistoricalEvent(event: "George H. W. Bush", date: 1989)
+let billClinton = HistoricalEvent(event: "Bill Clinton", date: 1993)
+let georgeWBush = HistoricalEvent(event: "George WBush", date: 2001)
+let barackObama = HistoricalEvent(event: "Barack Obama", date: 2009)
+
+
+
+var appleProducts = [georgeWashington, johnAdams, thomasJefferson, jamesMadison, jamesMonroe, johnQuincyAdams, andrewJackson,martinVanBuren, williamHenryHarrison, johnTyler, jamesKPolk, zacharyTaylor, millardFillmore, franklinPierce, jamesBuchanan, abrahamLincoln, andrewJohnson, ulyssesSGrant, rutherfordBHayes, jamesAGarfield, chesterAArthur, groverCleveland1, benjaminHarrison, groverCleveland2, williamMckinley, theodoreRoosevelt, williamHowardTaft, woodrowWilson, warrenGHarding, calvinCoolidge, herbertHoover, franklinDRoosevelt, harrySTruman, dwightDEisenhower, johnFKennedy, lyndonBJohnson, richardNixon, geraldFord,jimmyCarter, ronaldReagan, georgeHWBush, billClinton, georgeWBush,barackObama]
+
+
+
--- BoutTime/ViewController.swift
+++ BoutTime/ViewController.swift
@@ -7,19 +7,218 @@
 //
 
 import UIKit
+import GameplayKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var firstEventLable: UILabel!
+    @IBOutlet weak var secondEventLable: UILabel!
+    @IBOutlet weak var thirdEventLable: UILabel!
+    @IBOutlet weak var forthEventLable: UILabel!
+    @IBOutlet weak var firstDownButton: UIButton!
+    @IBOutlet weak var secondUpButton: UIButton!
+    @IBOutlet weak var secondDownButton: UIButton!
+    @IBOutlet weak var thirdUpButton: UIButton!
+    @IBOutlet weak var thirdDownButton: UIButton!
+    @IBOutlet weak var forthUpButton: UIButton!
+    @IBOutlet weak var countDownLabel: UILabel!
+    @IBOutlet weak var nextRoundButton: UIButton!
+    
+    var soundEffect: AVAudioPlayer!
+    var gameScore = 0
+    var roundCounter = 0
+    var countDown = 60
+    var timer: NSTimer?
+    var gameEvents : [HistoricalEvent] = []
+    var currentRound : [HistoricalEvent] = []
+    var nextGame: [HistoricalEvent] = []
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        startGame()
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    //Shake motion code
+    override func canBecomeFirstResponder() -> Bool {
+        return true
+    }
+    
+    override func motionEnded(motion: UIEventSubtype, withEvent event: UIEvent?) {
+        if motion == .MotionShake {
+            checkAnswer(currentRound)
+        }
+    }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject!) {
+        if segue.identifier == "gameOver" {
+            let svg = segue.destinationViewController as! EndGameController
+            
+            svg.endGameScore = "\(gameScore)/4"
+        }
+    }
 
+    // Get 24 random questions from the appleProducts Array located in the HistoricalEvent.swift file
+    func startGame() {
+        shuffle()
+        for i in 0...23 {
+            gameEvents.append(nextGame[i])
+        }
+        
+        startRound()
+    }
+    
+    func startRound() {
+        timer = NSTimer.scheduledTimerWithTimeInterval(1.0, target: self, selector: #selector(ViewController.startTimer), userInfo: nil, repeats: true)
+        
+        for i in 0...3 {
+            currentRound.append(gameEvents[i])
+            //set the UITextView to each event
+        }
+        firstEventLable.text = currentRound[0].description
+        secondEventLable.text = currentRound[1].description
+        thirdEventLable.text = currentRound[2].description
+        forthEventLable.text = currentRound[3].description
+    }
+    
+    func resetCounterLabel(){
+        countDown = 60
+        countDownLabel.text = "0:\(countDown)"
+    }
+    
+    func startTimer() {
+        countDown -= 1
+        countDownLabel.text = "0:\(countDown)"
+        
+        if countDown < 10 {
+            countDownLabel.text = "0:0\(countDown)"
+        }
+        
+        if countDown == 0 {
+            timer!.invalidate()
+            checkAnswer(currentRound)
+        }
+    }
+    
+    
+    //This checks to see what button was pushed to determine what elements in the array need to be switched and then updates the lables
+    @IBAction func upOrDown(sender: AnyObject) {
+        switch sender.tag {
+        case 1:
+            swap(&currentRound[0], &currentRound[1])
+        case 2:
+            swap(&currentRound[1], &currentRound[0])
+        case 3:
+            swap(&currentRound[1], &currentRound[2])
+        case 4:
+            swap(&currentRound[2], &currentRound[1])
+        case 5:
+            swap(&currentRound[2], &currentRound[3])
+        case 6:
+            swap(&currentRound[3], &currentRound[2])
+        default:
+            print("defualt")
+        }
+        updateEventLables()
+    }
+    
+    //Check users answer by comparing the users answer to the users sorted answer
+    func checkAnswer(Useranswer: [HistoricalEvent]) {
+        timer!.invalidate()
+        resetCounterLabel()
+        roundCounter += 1
+        
+        if roundCounter == 6 {
+            endGame()
+            return
+        }
+        
+        //sort users answer. In short we are putting all the events in order
+        let answer = Useranswer.sort{$0.date < $1.date}
+        
+        if currentRound == answer {
+            // Add 1 point for given round
+            gameScore += 1
+            nextRoundButton.setImage(UIImage(named: "next_round_success.png"), forState: UIControlState.Normal)
+            playSound("CorrectDing.wav")
+        } else {
+            // Let the user know they are wrong! Wrong! WRONG!
+            playSound("IncorrectBuzz.wav")
+            nextRoundButton.setImage(UIImage(named: "next_round_fail.png"), forState: UIControlState.Normal)
+        }
+        countDown = 60
+        endRound()
+        showNextRoundButton()
+        print(gameScore)
+    }
+        //Updates label text
+    func updateEventLables() {
+        firstEventLable.text = currentRound[0].description
+        secondEventLable.text = currentRound[1].description
+        thirdEventLable.text = currentRound[2].description
+        forthEventLable.text = currentRound[3].description
+    }
+    
+        //Shuffles the array so that each game is distinctive
+    func shuffle() {
+        nextGame = GKRandomSource.sharedRandom().arrayByShufflingObjectsInArray(appleProducts) as! [HistoricalEvent]
+    }
+    
+        //Remove the last 4 items from the current array
+    func endRound() {
+        gameEvents.removeRange(0...3)
+        
+    }
+    
+    
+    func showNextRoundButton() {
+        nextRoundButton.hidden = false
+    }
+    
+    @IBAction func nextRoundButtonPressed(sender: AnyObject) {
+        nextRoundButton.hidden = true
+        clearCurrentRound()
+        startRound()
+        
+    }
+    
+        //Clear the current round array after which you will need to call startRound() to add the next events for the following round.
+    func clearCurrentRound() {
+        currentRound.removeAll()
+    }
+    
+        //When the user shakes the iPhone we want to check the answer
+    func userShakesiPhone(){
+        checkAnswer(currentRound)
+    }
+    
+    func playSound(file: String) {
+        let path = NSBundle.mainBundle().pathForResource(file, ofType:nil)!
+        let url = NSURL(fileURLWithPath: path)
+        
+        do {
+            let sound = try AVAudioPlayer(contentsOfURL: url)
+            soundEffect = sound
+            sound.play()
+        } catch {
+            print("File not found")
+        }
+    }
+    
+        // Game over! display user score
+    func endGame() {
+        roundCounter = 0
+        self.performSegueWithIdentifier("gameOver", sender: self)
+    }
 
 }
 
